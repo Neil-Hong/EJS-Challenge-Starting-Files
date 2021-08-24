cc90b8ed019b44dcdc9b891864945a79d56b3dad
Neil Hong <807312043@qq.com>
2021-08-24 19:21:30 +1000

    done

--- ejs-challenge/app.js
+++ ejs-challenge/app.js
@@ -12,20 +12,60 @@ const app = express();
 
 app.set('view engine', 'ejs');
 
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
 app.use(express.static("public"));
 
-app.get("/",function(req.res){
-  res.render("home");
+let posts = [];
+const _ = require("lodash");
+
+app.get("/", function(req, res) {
+  res.render("home", {
+    startingContent: homeStartingContent,
+    posts: posts
+  });
+
 })
 
+app.get("/about", function(req, res) {
+  res.render("about", {
+    aboutContent: aboutContent
+  });
+})
 
+app.get("/contact", function(req, res) {
+  res.render("contact", {
+    contactContent: contactContent
+  });
+})
 
+app.get("/compose", function(req, res) {
+  res.render("compose");
+})
 
+app.post("/compose", function(req, res) {
+  const post = {
+    title: req.body.postTitle,
+    content: req.body.postBody
+  };
+  posts.push(post);
+  res.redirect("/");
+})
 
+app.get("/posts/:postName", function(req, res){
+  const requestedTitle = _.lowerCase(req.params.postName);
 
-
-
+  posts.forEach(function(post){
+    const storedTitle = _.lowerCase(post.title);
+    if(storedTitle === requestedTitle){
+      res.render("post", {
+        title: post.title,
+        content: post.content
+      })
+    }
+  })
+})
 
 
 
